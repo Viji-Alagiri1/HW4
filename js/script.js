f75337ed2d5ec3e16601e53162b37baa16d2c9a6
Viji-Alagiri1 <va663@nyu.edu>
2016-03-19 18:26:18 -0400

    newtst

--- js/script.js
+++ js/script.js
@@ -1,45 +1,3 @@
-var myNumber = 1;
-var riverNames = [
-'East River',
-'Flushing River',
-'Harlem River',
-'Hudson River',
-];
-var riverImages = [
-'http://www.citylandnyc.org/wp-content/uploads/sites/14/2011/03/EastRiver-regular.jpg',
-'http://c2.staticflickr.com/2/1378/5107251279_797621253a_b.jpg',
-'http://www.kiewit.com/files/cache/bc2788f9c1ef41b31e3029a254871da1_f1278.JPG',
-'http://media-2.web.britannica.com/eb-media/12/132112-004-F18EB014.jpg',
-];
-
-
-// update function that will be called within click functions 
-// (foward and back)
-// carefule: indexing is zero-based like in Python!
-function updateEverything(currentNumber) {
-  $('#riverImage').attr('src',riverImages[currentNumber-1]);
-  $('#riverName').text(riverNames[currentNumber-1]);
-  $('#myNumber').text(currentNumber);
-}
-
-$('#forward').click( function() {
-  if (myNumber < 4) {
-    myNumber = myNumber + 1;
-  } else {
-    myNumber = 1;
-  }
-  updateEverything(myNumber)
-});
-
-$('#back').click( function() {
-  if (myNumber > 1) {
-    myNumber = myNumber - 1;
-  } else {
-    myNumber = 4;
-  }
-  $('#myNumber').text(myNumber);
-    updateEverything(myNumber)
-});
 
   var basemapUrl = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
   var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
@@ -77,6 +35,9 @@ $('#back').click( function() {
       fillOpacity: 0.8
     };
 
+
+
+
     L.geoJson(data, 
     {
       //calling L.geoJson with pointToLayer as an option will automatically add markers to the map from our data
@@ -93,4 +54,160 @@ $('#back').click( function() {
     }
     ).addTo(map1);
 
+
+
+
+
+
+
   });
+
+  //map2 simply shows a geojson layer with polygons using NYC NTA data
+
+  //initialize map2
+  var map2 = L.map('map2', {
+    scrollWheelZoom: false
+  }).setView( [40.767802,-73.953266], 12);
+  
+  //CartoDB Basemap
+  L.tileLayer(basemapUrl,{
+    attribution: attribution
+  }).addTo(map2);
+
+
+  $.getJSON('data/neighborhoods.geojson', function(nabe_data) {
+    L.geoJson(nabe_data).addTo(map2);
+  })
+
+
+  //map 3 is a rebuild of this leaflet choropleth demo: http://leafletjs.com/examples/choropleth.html
+
+  //initialize map3
+  var map3 = L.map('map3', {
+    scrollWheelZoom: false
+  }).setView( [40.706913,-73.987513], 5);
+
+  //CartoDB Basemap
+  L.tileLayer(basemapUrl,{
+    attribution: attribution
+  }).addTo(map3);
+
+  var geojson;
+
+  //this function takes a value and returns a color based on which bucket the value falls between
+  function getColor(d) {
+      return d > 1000 ? '#0000cc' :
+             d > 500  ? '#BD0026' :
+             d > 200  ? '#E31A1C' :
+             d > 100  ? '#FC4E2A' :
+             d > 50   ? '#FD8D3C' :
+             d > 20   ? '#FEB24C' :
+             d > 10   ? '#FED976' :
+                        '#FFEDA0';
+  }
+
+  //this function returns a style object, but dynamically sets fillColor based on the data
+  function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+  }
+
+  //this function is set to run when a user mouses over any polygon
+  function mouseoverFunction(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+
+    //update the text in the infowindow with whatever was in the data
+    console.log(layer.feature.properties.name);
+    $('#infoWindow').text(layer.feature.properties.name);
+
+  }
+
+  //this runs on mouseout
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+  }
+
+  //this is executed once for each feature in the data, and adds listeners
+  function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: mouseoverFunction,
+        mouseout: resetHighlight
+        //click: zoomToFeature
+    });
+  }
+
+
+  //all of the helper functions are defined and ready to go, so let's get some data and render it!
+
+  //be sure to specify style and onEachFeature options when calling L.geoJson().
+  $.getJSON('data/states.geojson', function(state_data) {
+    geojson = L.geoJson(state_data,{
+      style: style,
+      onEachFeature: onEachFeature
+    }).addTo(map3);
+  });
+
+  //initialize map4
+  var map4 = L.map('map4', {
+    scrollWheelZoom: false
+  }).setView( [40.706913,-73.987513], 5);
+
+  //CartoDB Basemap
+  L.tileLayer(basemapUrl,{
+    attribution: attribution
+  }).addTo(map4);
+
+  //load external geojson
+  $.getJSON('data/cities.geojson', function(data) {
+    console.log(data);
+
+    var burgerIcon = L.icon({
+      iconUrl: 'img/burger.png',
+      iconSize:     [37, 37], // size of the icon
+      iconAnchor:   [16, 37] // point of the icon which will correspond to marker's location
+    });
+    var lawnMowerIcon = L.icon({
+      iconUrl: 'img/lawnmower.png',
+      iconSize:     [37, 37], // size of the icon
+      iconAnchor:   [16, 37] // point of the icon which will correspond to marker's location
+    });
+
+    L.geoJson(data, 
+    {
+      //calling L.geoJson with pointToLayer as an option will automatically add markers to the map from our data
+      pointToLayer: function (feature, latlng) {
+
+          console.log(feature);
+
+          if(feature.properties.chris_lived_here == "true") {
+            return L.marker(latlng, {icon: burgerIcon})
+              .bindPopup('Chris has lived in ' + feature.properties.name);
+          } else {
+            return L.marker(latlng, {icon: lawnMowerIcon})
+            .bindPopup('Chris has not lived in ' + feature.properties.name);;
+          }
+      }
+    }
+    ).addTo(map4);
+
+
+
+  })
+ 
