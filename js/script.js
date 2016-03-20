778f8e5d02482e7aab000622d01df9b401e43a7b
Viji-Alagiri1 <va663@nyu.edu>
2016-03-20 10:27:00 -0400

    ok

--- js/script.js
+++ js/script.js
@@ -100,22 +100,31 @@ var myMapData = [
   $.getJSON('data/cities.geojson', function(data) {
     console.log(data);
 
+    //icon to point river location
     var riverIcon = L.icon({
       iconUrl: 'img/river.png',
       iconSize:     [37, 37], // size of the icon
       iconAnchor:   [16, 37] // point of the icon which will correspond to marker's location
     });
+    //icon to point PARK location
     var parkIcon = L.icon({
       iconUrl: 'img/ferriswheel.png',
       iconSize:     [37, 37], // size of the icon
       iconAnchor:   [16, 37] // point of the icon which will correspond to marker's location
     });
-
+    //icon to point Mountain location
     var mountainsIcon = L.icon({
       iconUrl: 'img/mountains.png',
       iconSize:     [37, 37], // size of the icon
       iconAnchor:   [16, 37] // point of the icon which will correspond to marker's location
     });
+    //icon to point to the places I have been to
+    var visitedIcon = L.icon({
+      iconUrl: 'img/sight-2.png',
+      iconSize:     [37, 37], // size of the icon
+      iconAnchor:   [16, 37] // point of the icon which will correspond to marker's location
+    });
+    //icon to point to the places I Plan on Visiting
     var futureIcon = L.icon({
       iconUrl: 'img/palm.png',
       iconSize:     [37, 37], // size of the icon
@@ -128,15 +137,27 @@ var myMapData = [
       pointToLayer: function (feature, latlng) {
 
           console.log(feature);
-          //places I visited
+          //places I visited had beautiful river-front
           if(feature.properties.river == "true") {
             return L.marker(latlng, {icon: riverIcon})
               .bindPopup('I visited a beautiful river in ' + feature.properties.name);
           } 
+          //parks that visited and had fun
           else if(feature.properties.park == "true") {
             return L.marker(latlng, {icon: parkIcon})
+              .bindPopup('I had lots of fun in this ' + feature.properties.name  + ' amusement park.');
+          } 
+          //mountains that I drove on
+          else if(feature.properties.mountains == "true") {
+            return L.marker(latlng, {icon: mountainsIcon})
               .bindPopup('I had lots of fin in this ' + feature.properties.name  + ' amusement park.');
-          } //My Future Road Trips
+          } 
+          //simply the places I visited for vacations
+          else if(feature.properties.visited == "true") {
+            return L.marker(latlng, {icon: visitedIcon})
+              .bindPopup('I had good memories of visiting this ' + <b>feature.properties.name</b>  + ' place for vacation.');
+          } 
+          //places that I plan on visiting in future: "My Future Road Trips"
           else {
             return L.marker(latlng, {icon: futureIcon})
             .bindPopup('I plan on visiting this ' + feature.properties.name);;
