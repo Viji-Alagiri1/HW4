7a0bb43cec1978f05355749177c200d183e5024e
Viji-Alagiri1 <va663@nyu.edu>
2016-03-27 14:23:50 -0400

    hw5 final try

--- js/script.js
+++ js/script.js
@@ -1,47 +1,11 @@
-var myNumber = 1;
-var riverNames = [
-'East River',
-'Flushing River',
-'Harlem River',
-'Hudson River',
-];
-var riverImages = [
-'http://www.citylandnyc.org/wp-content/uploads/sites/14/2011/03/EastRiver-regular.jpg',
-'http://c2.staticflickr.com/2/1378/5107251279_797621253a_b.jpg',
-'http://www.kiewit.com/files/cache/bc2788f9c1ef41b31e3029a254871da1_f1278.JPG',
-'http://media-2.web.britannica.com/eb-media/12/132112-004-F18EB014.jpg',
-];
+$(document).ready(function(e){
+        $(".img-check").click(function(){
+        $(this).toggleClass("check");
+      });
+  });
 
-// update function that will be called within click functions 
-// (foward and back)
-// carefule: indexing is zero-based like in Python!
-function updateEverything(currentNumber) {
-  $('#riverImage').attr('src',riverImages[currentNumber-1]);
-  $('#riverName').text(riverNames[currentNumber-1]);
-  $('#myNumber').text(currentNumber);
-}
-
-$('#forward').click( function() {
-  if (myNumber < 4) {
-    myNumber = myNumber + 1;
-  } else {
-    myNumber = 1;
-  }
-  updateEverything(myNumber)
-});
-
-$('#back').click( function() {
-  if (myNumber > 1) {
-    myNumber = myNumber - 1;
-  } else {
-    myNumber = 4;
-  }
-  $('#myNumber').text(myNumber);
-    updateEverything(myNumber)
-});
-
-  var basemapUrl = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
-  var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+var basemapUrl = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
+var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
 
 var myMapData = [
     {
@@ -135,6 +99,8 @@ var myMapData = [
       iconAnchor:   [16, 37] // point of the icon which will correspond to marker's location
     });
 
+  
+  
     L.geoJson(data, 
     {
       //calling L.geoJson with pointToLayer as an option will automatically add markers to the map from our data
